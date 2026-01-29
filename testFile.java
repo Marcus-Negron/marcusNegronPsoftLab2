2f03938d05046470b200f9076cb18c2ed0de3ba7
shipiy28 <shipiy@rpi.edu>
2026-01-28 19:17:10 -0500

    Update testFile.java

--- testFile.java
+++ testFile.java
@@ -1,6 +1,7 @@
 public class TestFile{
     public static void main(String[] args){
         System.out.println("Hello to the world!!");
-        Systeml.out.println("Hi Marcus! Wanna play fortnite?");
+        System.out.println("Hi Marcus! Wanna play fortnite?");
+        System.out.println("Sure, sounds good.");
     }
 }
